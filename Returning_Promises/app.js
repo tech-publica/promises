01b42a91e9031ad04a09a36adf9e6694186cd1b5
ric <riccardo.at.work@gmail.com>
2022-01-26 11:23:02 -0500

    first commit

---
+++ Returning_Promises/app.js
@@ -0,0 +1,22 @@
+const makeDogPromise = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const rand = Math.random();
+			if (rand < 0.5) {
+				resolve();
+			}
+			else {
+				reject();
+			}
+		}, 5000);
+	});
+};
+
+let dogPromise = makeDogPromise();
+dogPromise
+	.then(() => {
+		console.log('YAY WE GOT A DOG!!!!');
+	})
+	.catch(() => {
+		console.log(':( NO DOG');
+	});
